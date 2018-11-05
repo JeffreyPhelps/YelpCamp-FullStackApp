733adff5b460b6636178ddcd45b26309dcbf2b67
Jeffrey <phelpsjeffrey@yahoo.com>
2018-11-04 18:47:00 -0700

    Creating Campgrounds Routes and Page

--- app.js
+++ app.js
@@ -8,6 +8,21 @@
 const express = require("express");
 const app = express();
 
+// Initializing body-parser NPM package
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+
+// Creating initial campgrounds array and variable
+let campgroundsArray = [
+    {name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2016/11/29/04/17/bonfire-1867275_960_720.jpg"},
+    {name: "Granite Hill", image: "https://cdn.pixabay.com/photo/2015/07/10/17/24/night-839807_960_720.jpg"},
+    {name: "Goat's Rest", image: "https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg"}
+]
+
 
 
 
@@ -21,17 +36,23 @@ app.get("/", function(req, res){ // Using express to create HTTP route, with req
 
 // Campgrounds Route
 app.get("/campgrounds", function(req, res){ // Using express to create HTTP route, with required callback
-
-    let campgroundsArray = [
-        {name: "Salmon Creek", image: "https://cdn.pixabay.com/photo/2016/11/29/04/17/bonfire-1867275_960_720.jpg"},
-        {name: "Granite Hill", image: "https://cdn.pixabay.com/photo/2015/07/10/17/24/night-839807_960_720.jpg"},
-        {name: "Goat's Rest", image: "https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_960_720.jpg"}
-    ]
-
     res.render("campgrounds.ejs", {campgrounds:campgroundsArray}); // Rendering the campgrounds file from the views folder
 });                                                                // and the campgroundsArray as "campgrounds" variable
 
 
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("newcamp.ejs");
+});
+
+
+app.post("/campgrounds", function(req, res){
+    let name = req.body.name;
+    let image = req.body.image;
+    let newCampground = {name: name, image: image};
+    campgroundsArray.push(newCampground);
+    res.redirect("/campgrounds");
+});
 
 
 
